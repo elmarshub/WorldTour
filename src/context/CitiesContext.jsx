a46aa8f94f54bc4c5f8a8ff51f3d1ec5cd87dd41
Martin Ifeanyi <martinsifeanyi247@gmail.com>
2025-01-03 16:40:37 +0100

    complete

--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -1,4 +1,10 @@
-import { createContext, useReducer, useEffect, useContext } from "react";
+import {
+  createContext,
+  useReducer,
+  useCallback,
+  useEffect,
+  useContext,
+} from "react";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -80,23 +86,26 @@ function CitiesProvider({ children }) {
   }, []);
 
   // Fetch a single city by its ID
-  async function getCity(id) {
-    // If the city is already loaded, don't fetch it again
-    if (Number(id) === currentCity.id) return;
+  const getCity = useCallback(
+    async function getCity(id) {
+      // If the city is already loaded, don't fetch it again
+      if (Number(id) === currentCity.id) return;
 
-    dispatch({ type: "loading" });
+      dispatch({ type: "loading" });
 
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: "city/loaded", payload: data });
-    } catch {
-      dispatch({
-        type: "rejected",
-        payload: "There was an error loading the city...",
-      });
-    }
-  }
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: "city/loaded", payload: data });
+      } catch {
+        dispatch({
+          type: "rejected",
+          payload: "There was an error loading the city...",
+        });
+      }
+    },
+    [currentCity.id]
+  );
 
   // // Create a new city
   async function createCity(newCity) {
